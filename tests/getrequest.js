be3accb74273d0ea90e1e2bc67df5ce807dcb055
darshandnagesh <darshan.nagesh@sdworx.com>
2025-04-06 21:16:42 +0530

    Import textSummary for enhanced reporting in performance tests

--- tests/getrequest.js
+++ tests/getrequest.js
@@ -1,7 +1,7 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
-
+import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
 export let options = {
   vus: 1, // Number of virtual users
   duration: '5s', // Test duration
